793f5191330765689e89bc5ff94f18fa806cf1ff
Amir Marmul <amiruddinmarmul@gmail.com>
2024-09-23 17:56:16 +0800

    user created events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,28 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = current_user.created_events.new(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date)
+  end
 end
